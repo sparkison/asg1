ec864023e7b5ccd8e085adbd7367ae8fe518c72c
shaunpa <shaunpa@phoenix.cs.colostate.edu>
2015-02-13 11:55:31 -0700

    Took the lock out of TCPSender. Only locks in MessagingNode now.

--- CS455_ASG1/src/cs455/overlay/transport/TCPSender.java
+++ CS455_ASG1/src/cs455/overlay/transport/TCPSender.java
@@ -4,26 +4,23 @@ import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class TCPSender {
 	@SuppressWarnings("unused")
 	private Socket socket;
 	private DataOutputStream dout;
-	private ReentrantLock sendLock;
 
 	public TCPSender(Socket socket) throws IOException {
 		this.socket = socket;
-		sendLock = new ReentrantLock();
 		dout = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
 	}
-	
+
 	public void sendData(byte[] dataToSend) throws IOException {
 
-	        int dataLength = dataToSend.length;
-	        dout.writeInt(dataLength);
-	        dout.write(dataToSend, 0, dataLength);
-	        dout.flush();
-		
+		int dataLength = dataToSend.length;
+		dout.writeInt(dataLength);
+		dout.write(dataToSend, 0, dataLength);
+		dout.flush();
+
 	}
 }
